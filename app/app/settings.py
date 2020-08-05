479924df1251a1b7711562ef44ed1272cd907d6a
Srinivasa Gurajala <srinivasa.gurajala@anz.com>
2020-08-05 22:00:54 +0530

    add host

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e&7yubqk+0sm+i#f1iva271##b@rq1wh&%5)#4x&ei%li!s@pm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [react-python-app.herokuapp.com]
 
 
 # Application definition
